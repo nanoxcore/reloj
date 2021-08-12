e82eff110cd05d61d0ed9529aa20c7686b5d7f17
nanoxcore <44485663+nanoxcore@users.noreply.github.com>
2021-08-12 08:43:33 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+let segundos = 50
+let minutos = 57
+let horas = 0
 basic.forever(function () {
-	
+    if (segundos < 59) {
+        segundos += 1
+    } else {
+        segundos = 0
+    }
+    if (segundos == 0) {
+        minutos += 1
+    } else {
+        minutos = 0
+    }
+    if (horas < 12) {
+    	
+    }
 })
